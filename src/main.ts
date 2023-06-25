08dfff0a3d8338c76f0ddcf4124b682c8175af0f
gsemir0418 <845217811@qq.com>
2023-06-25 09:44:00 +0800

    feat: init Vue Router 4

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,19 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-
-createApp(App).mount('#app')
+import { createRouter, createWebHashHistory } from 'vue-router';
+import HelloWorld from './components/HelloWorld.vue'
+import Login from './views/Login.vue'
+// 路由文件
+const routes = [
+  { path: "/", component: HelloWorld  },
+  { path: "/login", component: Login },
+];
+// 传入路由配置
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
---
+++ src/views/Login.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+<template lang="">
+  <div>
+    login
+  </div>
+</template>
+<style lang="">
+  
+</style>
