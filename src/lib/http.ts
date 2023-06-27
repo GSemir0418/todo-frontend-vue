f6f1f8274936969d6ffb7f5d37fe8a033372a2b1
gsemir0418 <845217811@qq.com>
2023-06-27 17:52:23 +0800

    feat: http client based on axios

---
+++ src/lib/http.ts
@@ -0,0 +1,46 @@
+import axios, { AxiosInstance, AxiosRequestConfig } from "axios";
+
+export class Http {
+  instance: AxiosInstance
+  constructor(baseURL: string) {
+    this.instance = axios.create({ baseURL })
+  }
+
+  get<R = unknown>(
+    url: string,
+    query?: Record<string, string | number>,
+    config?: Omit<AxiosRequestConfig, 'url' | 'params' | 'method'>
+  ) {
+    return this.instance.request<R>({
+      ...config,
+      url,
+      params: query,
+      method: "get"
+    })
+  }
+
+  post<R = unknown>(
+    url: string,
+    data?: any,
+    config?: Omit<AxiosRequestConfig, "url" | "data" | "method">
+  ) {
+    return this.instance.request<R>({
+      ...config,
+      url,
+      data,
+      method: "post"
+    })
+  }
+
+}
+
+export const http = new Http('/dev')
+
+// 拦截器
+http.instance.interceptors.request.use((config) => {
+  const token = localStorage.getItem("jwt");
+  if (token) {
+    config.headers!.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
